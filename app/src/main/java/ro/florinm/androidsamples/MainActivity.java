5a45ffc64ea4a9e7925e5829cca0139361035433
Florin Muscalu <florin.muscalu@gmail.com>
2021-02-04 23:10:29 +0200

    Added auto-incrementing VERSION_CODE to the build.gradle file.

--- app/src/main/java/ro/florinm/androidsamples/MainActivity.java
+++ app/src/main/java/ro/florinm/androidsamples/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView ver = findViewById(R.id.text_version);
+        ver.setText("Version: " + BuildConfig.VERSION_NAME + "." + BuildConfig.VERSION_CODE);
     }
 
     public void click1(View view) {
